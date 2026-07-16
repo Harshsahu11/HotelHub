e38c41014e6d513a302276d1f214d1eb8affa547
Harsh Sahu <harshsahu1109@gmail.com>
2026-07-16 17:22:02 +0530

    Inventory service and Controller Implementation

---
+++ src/main/java/com/detrox/HotelHub/service/InventoryService.java
@@ -0,0 +1,5 @@
+package com.detrox.HotelHub.service;
+
+public interface InventoryService {
+
+}
---
+++ src/main/java/com/detrox/HotelHub/serviceImpl/InventoryServiceImpl.java
@@ -0,0 +1,16 @@
+package com.detrox.HotelHub.serviceImpl;
+
+import com.detrox.HotelHub.repository.InventoryRepository;
+import com.detrox.HotelHub.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class InventoryServiceImpl implements InventoryService {
+
+
+
+}
